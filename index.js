3122d72995135d9e7732d9aa8a31c8476a3b1d71
priyanshu24000 <priyanshu24000>
2024-07-07 09:54:20 +0530

    fix

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const cors = require('cors'); // Import the cors package
+const cors = require('cors');
 const registerRoute = require('./routes/register');
 const loginRoute = require('./routes/login');
-const getUserRoute = require('./routes/getUser')
+const getUserRoute = require('./routes/getUser');
 const jobsRoute = require('./routes/jobs');
-const removeJobRoute = require ('./routes/removeJob')
+const removeJobRoute = require('./routes/removeJob');
 const getJobsRoute = require('./routes/getJobs');
 const allowCors = require('./allowCors'); // Adjust the path if necessary
-const applyRoute = require('./routes/apply'); 
-const addBlogRoute = require('./routes/addBlog')
+const applyRoute = require('./routes/apply');
+const addBlogRoute = require('./routes/addBlog');
 const getBlogsRouter = require('./routes/getBlogs');
-
 const announcementsRoute = require('./routes/announcements');
 
 const app = express();
-const port = 3001;
 
 require('dotenv').config();
 
 // Middleware
-app.use(allowCors); // Apply allowCors middleware globally
-app.use(bodyParser.json()); // Parse JSON bodies
+app.use(allowCors);
+app.use(bodyParser.json());
+app.use(cors()); // Use CORS
 
 // Routes
 app.use('/register', registerRoute);
@@ -33,17 +32,21 @@ app.use('/removeJob', removeJobRoute);
 app.use('/getJobs', getJobsRoute);
 app.use('/jobs/apply', applyRoute);
 app.use('/addBlog', addBlogRoute);
-
 app.use('/getBlogs', getBlogsRouter);
-
 app.use('/announcements', announcementsRoute);
-
 
 /* MONGOOSE SETUP */
 const uri = process.env.MONGO_URL;
-mongoose
-  .connect(uri)
-  .then(() => {
-    app.listen(port, () => console.log(`Server Port: ${port}`));
-  })
-  .catch((error) => console.log(`${error} did not connect`));
+let mongooseConnection;
+
+async function connectToDatabase() {
+  if (!mongooseConnection) {
+    mongooseConnection = mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    await mongooseConnection;
+  }
+}
+
+module.exports = async (req, res) => {
+  await connectToDatabase();
+  app(req, res);
+};
